cd529eac78a4ddaeca90134a367cf00542160199
alex <derdiii@mail.ru>
2022-12-16 23:37:42 +0300

    first commit

---
+++ hw task 27/Program.cs
@@ -0,0 +1,22 @@
+﻿int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Razryd(int num)
+{
+    int count = 0;
+    while(num > 0)
+    {
+        count = count + num % 10;
+        num = num /10 ;
+    }
+    return count;
+    
+    
+}
+
+int A = ReadInt("Введите число: ");
+int result = Razryd(A);
+Console.WriteLine(result);
---
+++ hw task 29/Program.cs
@@ -0,0 +1,16 @@
+﻿int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] array = new int[8];
+
+for (int i = 0; i < 8; i++)
+            {
+                array[i] = ReadInt("Введите число: ");
+                
+            }
+foreach (int s in array) Console.Write(s+ " ");
+Console.ReadLine();
+
